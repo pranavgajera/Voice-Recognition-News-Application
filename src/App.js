1ca5a3c009953a5de84f93c066faf595cb32c2a3
Pranav Gajera <pranavgajera@gmail.com>
2020-08-19 20:37:08 -0400

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react'
+import wordstoNumbers from 'words-to-numbers';
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from './styles.js'
+const alanKey='API_KEY'
+const App =() =>{
+    const[activeArticle,setActiveArticle]=useState(-1);
+    const classes =useStyles();
+    const [newsArticles,setNewsArticles]=useState([])
+    useEffect(()=>{
+        alanBtn({
+            key: alanKey,
+            onCommand:({command,articles,number}) =>{
+                if(command ===  'newHeadlines'){
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                }else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle)=> prevActiveArticle+1);
+                }else if(command === 'open'){
+                    const parsedNumber = number.length > 2 ? wordstoNumbers(number,{fuzzy:true}) :number;
+                    const article =articles[parsedNumber-1];
+                    if(parsedNumber > 20){
+                        alanBtn().playText("Please try that again")
+                    }
+                    else if(article){
+                        window.open(article.url,'_blank');
+                        alanBtn().playText("Opening...")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                    }
+                }
+            }
+        })
+    },[])
+    return(
+        <div>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    )
 }
-
 export default App;
